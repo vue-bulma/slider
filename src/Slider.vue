a6c00292dfd3d83845da946888dec1fc7a362e8f
luventa <silentmic@qq.com>
2017-02-14 15:09:30 +0800

    fix: #3 and #4, avoid to emit change twice & enhancement

--- src/Slider.vue
+++ src/Slider.vue
@@ -17,7 +17,10 @@ export default {
       type: Number,
       default: 0.01
     },
-    value: Number,
+    value: {
+      type: Number,
+      default: 0
+    },
     type: String,
     name: String,
     size: String,
@@ -33,18 +36,40 @@ export default {
     }
   },
 
+  beforeMount () {
+    if (this.max < this.min) {
+      throw 'Unexpected range setting: Maximum cannot be less than minimum'
+    }
+
+    this.update(this.value)
+  },
+
   mounted () {
     this.$el.style.setProperty('--low', this.low)
     this.$el.style.setProperty('--high', this.high)
   },
 
   watch: {
-    realValue (val) {
-      this.$el.style.setProperty('--high', this.high)
-      this.$emit('change', val)
+    realValue (newVal, oldVal) {
+      if (Number(newVal) !== Number(oldVal)) {
+        this.$el.style.setProperty('--high', this.high)
+        this.$emit('change', newVal)
+      }
     },
     value (val) {
-      this.realValue = val
+      this.update(val)
+    }
+  },
+
+  methods: {
+    update (val) {
+      if (val > this.max) {
+        this.realValue = this.max
+      } else if (val < this.min) {
+        this.realValue = this.min
+      } else {
+        this.realValue = val
+      }
     }
   },
 
@@ -53,7 +78,7 @@ export default {
       return '0%'
     },
     high () {
-      return this.realValue / this.max * 100 + '%'
+      return (this.realValue - this.min) / (this.max - this.min) * 100 + '%'
     },
     classObject () {
       const { type, size, isFullwidth } = this
