e711fc025e8574831ab2bd898eda6c384cd93d92
Roland Schütz <mail@rolandschuetz.at>
2019-03-12 09:36:20 +0100

    TASK: Support Bulma >= 0.4.1

--- src/Slider.vue
+++ src/Slider.vue
@@ -94,7 +94,9 @@ export default {
 </script>
 
 <style lang="scss">
-@import '~bulma/sass/utilities/variables';
+@import '~bulma/sass/utilities/initial-variables';
+@import '~bulma/sass/utilities/functions';
+@import '~bulma/sass/utilities/derived-variables';
 
 input[type=range].slider {
   $radius: 290486px;
